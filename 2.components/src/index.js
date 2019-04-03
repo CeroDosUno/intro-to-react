49a599895a4390fc86a2631ee6240e38608558b8
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-03 02:47:49 -0700

    added content in index.js

--- 2.components/src/index.js
+++ 2.components/src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 
 const App = () =>{
   return (
-    <div>
-    Hi There
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src="" alt="avatar"/>
+        </a>
+        <div classname = "content">
+          <a href="/" className="author">
+          Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00 P.M.</span>
+          </div>
+          <div className="text">
+            Nice blog post
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 ReactDOM.render(
