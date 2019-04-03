8c3dfa990ac13be028c0b6c965cd885dfd3d1595
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-03 06:07:54 -0700

    refactored index.js by passing in props.

--- 2.components/src/CommentDetail.js
+++ 2.components/src/CommentDetail.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import faker from 'faker';
 
 
 const CommentDetail = (props) => {
@@ -9,17 +8,18 @@ const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img src={faker.image.avatar()} alt="avatar"/>
+        <img src={props.avatarpic} alt="avatar"/>
       </a>
       <div className = "content">
         <a href="/" className="author">
           {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00 P.M.</span>
+          <span className="date">
+          {props.currentime}</span>
         </div>
         <div className="text">
-          Nice blog post
+          {props.comment}
         </div>
       </div>
     </div>
--- 2.components/src/index.js
+++ 2.components/src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
+
+const commenttext= "great job!";
+const fakerimages = [faker.image.avatar(),
+  faker.image.avatar(),
+  faker.image.avatar()];
 
 const App = () =>{
   return (
     <div className="ui container comments">
-    <CommentDetail author="Sam"/>
-    <CommentDetail author="tammy"/>
-    <CommentDetail author="nancy"/>
+    <CommentDetail author="Sam"
+    currentime= "Today at 4:45 a.m."
+    comment = {commenttext}
+    avatarpic={fakerimages[1]}
+    />
+
+    <CommentDetail author="tammy"
+    currentime= "Today at 4:45 a.m."
+    comment = {commenttext}
+    avatarpic={fakerimages[0]}
+        />
+
+    <CommentDetail author="nancy"
+    currentime= "Today at 4:45 a.m."
+    comment = {commenttext}
+    avatarpic={fakerimages[2]}
+
+    />
 
     </div>
 
