f2696fa15a2c73ea1df32a7cf3d0ebb9d1a0a9fc
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-05 01:01:29 -0700

    completed project.

--- 7.songviewer/src/components/SongList.js
+++ 7.songviewer/src/components/SongList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 //add connect to see song selected
 import {connect} from 'react-redux';
+import {selectSong} from '../actions';
+
 
 class SongList extends React.Component{
   renderList(){
@@ -8,7 +10,9 @@ class SongList extends React.Component{
       return(
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">
+            <button className="ui button primary"
+            onClick={() => this.props.selectSong(song)}
+            >
             Select
             </button>
           </div>
@@ -16,20 +20,18 @@ class SongList extends React.Component{
           <div className="content">
           {song.title}
           </div>
-
         </div>
-
-
       );
     });
   }
 
 
   render(){
-    console.log(this.props.songs);
-
+    // console.log(this.props);
     return(
-      <div> {this.renderList()} </div>
+      <div className ="ui divided list">
+      {this.renderList()}
+      </div>
     );
   };
 }
@@ -38,9 +40,8 @@ class SongList extends React.Component{
 //state = all songs in store because there exists a value called songs
 //and have that show up as props to SongList
 const mapStateToProps = (state) =>{
-  return {
-    songs:state.songs
-  };
+  // console.log(state);
+  return {songs:state.songs};
 };
 
 
@@ -51,4 +52,15 @@ connect()()
 first parenthesis returns the function while the second parenthesis
 invokes the function
 */
-export default connect(mapStateToProps)(SongList);
+// Note that the action selectSong is not the same.
+// the connect function turns the selectSong
+// into store.dispatch(selectSong(song));
+
+export default connect(mapStateToProps, {
+  selectSong
+})(SongList);
+
+
+
+
+//
