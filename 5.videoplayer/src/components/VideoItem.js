9efef045fbe7cedac773269ded34b18b77f63411
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-04 19:23:59 -0700

    finished fixing errors for video selected.

--- 5.videoplayer/src/components/VideoItem.js
+++ 5.videoplayer/src/components/VideoItem.js
@@ -8,7 +8,8 @@ const VideoItem = ({video, onVideoSelect}) =>{
     className="video-item item">
       <img
       className="ui image"
-      src={video.snippet.thumbnails.medium.url}/>
+      src={video.snippet.thumbnails.medium.url}
+      alt={video.snippet.title}/>
 
       <div className="content">
         <div className="header">
--- 5.videoplayer/src/components/VideoList.js
+++ 5.videoplayer/src/components/VideoList.js
@@ -5,7 +5,8 @@ const VideoList = ({videos,onVideoSelect }) =>{
     const renderedList = videos.map( video => {
       return <VideoItem
       onVideoSelect={onVideoSelect}
-      video={video}/>;
+      video={video}
+      key={video.id.videoId}/>;
     });
 
   return(
