42e561c701ef6533578ee0661954eecab63d74f7
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-06 00:00:05 -0700

    added in posts rendering in postlist.js

--- 8.blogposts/src/components/PostList.js
+++ 8.blogposts/src/components/PostList.js
@@ -8,6 +8,8 @@ class PostList extends React.Component{
   }
 
   render(){
+    console.log(this.props.posts);
+
     return(
       <div>
         Post List
@@ -16,6 +18,8 @@ class PostList extends React.Component{
   }
 }
 
+const mapStateToProps = (state) =>{
+  return {posts: state.posts};
+};
 
-
-export default connect(null, {fetchPosts})(PostList);
+export default connect(mapStateToProps, {fetchPosts})(PostList);
--- 8.blogposts/src/reducers/postReducer.js
+++ 8.blogposts/src/reducers/postReducer.js
@@ -1,3 +1,13 @@
-export default () => {
-  return 123;
+export default (state=[], action) => {
+
+//always use switch statements to handle the action
+  switch(action.type){
+
+    case 'FETCH_POSTS':
+        return action.payload;
+
+    default:
+      return state;
+  }
+
 };
