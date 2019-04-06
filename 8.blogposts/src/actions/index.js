6865a55f992e140e3517b99bd559fae1dd0f00c6
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-06 00:40:55 -0700

    action creator to fetch userID made. error in the works. im on it

--- 8.blogposts/src/actions/index.js
+++ 8.blogposts/src/actions/index.js
@@ -1,14 +1,22 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 //DOTHIS
 //all this does is defining a function that returns a function
-export const fetchPosts = () =>
-   async (dispatch) => {
+export const fetchPosts = () =>   async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
 
       dispatch({
         type: 'FETCH_POSTS',
         payload: response.data
     })
+};
+
+export const fetchUser = (id) => async (dispatch) =>{
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({
+    type: 'FETCH_USER',
+    payload: response.data
+  })
 };
 
 
