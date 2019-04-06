d0d8443b59b69ca2511c1f50c6e34686be266b99
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-05 21:45:08 -0700

    added in redux thunk to handle API calls to jsoneverything api.

--- 8.blogposts/src/actions/index.js
+++ 8.blogposts/src/actions/index.js
@@ -1,5 +1,21 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+//DOTHIS
+//all this does is defining a function that returns a function
+export const fetchPosts = () =>
+   async (dispatch) => {
+    const response = await jsonPlaceholder.get('/posts');
 
+      dispatch({
+        type: 'FETCH_POSTS',
+        payload: response
+    })
+};
+
+
+
+/*
+//CANNOTDOTHIS
+//middleware is needed to make the API call
 export const fetchPosts = async () => {
   const response = await jsonPlaceholder.get('/posts');
 
@@ -8,3 +24,5 @@ export const fetchPosts = async () => {
       payload: response
   };
 };
+
+*/
