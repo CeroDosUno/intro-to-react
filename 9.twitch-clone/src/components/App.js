6c2fc76b813ff0807a6abe768b4de5bfbde31ab6
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-06 18:51:43 -0700

    more notes on react routers

--- 9.twitch-clone/src/components/App.js
+++ 9.twitch-clone/src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 const PageOne = () =>{
   return( <div>
     pg1
+    <Link to="/pagetwo"> Page2 </Link>
+
+    </div>);
+};
+
+const PageTwo = () =>{
+  return( <div>
+    PageTwo
+    !!BAD
+    <Link to="/"> Page1 </Link>
     </div>);
 };
 
@@ -13,6 +23,7 @@ const App = () =>{
       <BrowserRouter>
         <div>
           <Route path="/" exact component={PageOne}/>
+          <Route path="/pagetwo" exact component={PageTwo}/>
         </div>
       </BrowserRouter>
     </div>
