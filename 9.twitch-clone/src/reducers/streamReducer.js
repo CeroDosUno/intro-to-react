f13d15de7ee35dff0b9e93bbb1524e06adaac387
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-08 08:47:08 -0700

    finsihed streamreducer.

--- 9.twitch-clone/src/reducers/streamReducer.js
+++ 9.twitch-clone/src/reducers/streamReducer.js
@@ -7,6 +7,8 @@ import {
   DELETE_STREAM,
   EDIT_STREAM
 } from '../actions/types';
+import _ from 'lodash';
+
 
 export default (state = {}, action) =>{
   switch(action.type):
@@ -14,10 +16,10 @@ export default (state = {}, action) =>{
       return {...state, [action.payload.id] = action.payload};
     case FETCH_STREAM:
       return {...state, [action.payload.id] = action.payload};
-    case DELETE_STREAM:
-      return state.filter{stream => stream.userId === action.payload.id};
     case EDIT_STREAM:
       return {...state, [action.payload.id] = action.payload};
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
     default:
       return state;
 };
