4d772d1f945018d96a70086166bba0fa90d94796
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-08 14:35:35 -0700

    added create option in StreamList.

--- 9.twitch-clone/src/components/streams/StreamList.js
+++ 9.twitch-clone/src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {fetchStreams} from '../../actions';
+import {Link} from 'react-router-dom';
 
 class StreamList extends React.Component{
   componentDidMount() {
@@ -19,6 +20,18 @@ class StreamList extends React.Component{
         </button>
         </div>
       );
+    }
+  }
+
+  renderCreate(){
+    if(this.props.isSignedIn){
+        return(
+          <div style={{textAlign:'right'}}>
+            <Link to="/streams/new" className="ui button primary">
+              Create Stream
+            </Link>
+          </div>
+        );
     }
   }
 
@@ -50,6 +63,7 @@ class StreamList extends React.Component{
           <div className="ui celled list">
             {this.renderList()}
           </div>
+          {this.renderCreate()}
         </div>
       );
   }
@@ -60,7 +74,8 @@ const mapStateToProps = (state) =>{
   //object becomes array of objects
     return {
       streams: Object.values(state.streams),
-      currentUserId: state.auth.userId
+      currentUserId: state.auth.userId,
+      isSignedIn: state.auth.isSignedIn
     };
 };
 
