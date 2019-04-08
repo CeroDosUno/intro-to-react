56f145cc9527f62eae4e9e0966786812561abf32
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-08 05:50:59 -0700

    made all action creator types.

--- 9.twitch-clone/src/actions/index.js
+++ 9.twitch-clone/src/actions/index.js
@@ -1,8 +1,16 @@
 import streams from '../apis/streams';
 
-import {SIGN_IN, SIGN_OUT, CREATE_STREAM} from './types';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from './types';
 
-export const signIn = (userId) =>{
+export const signIn = (userId) => {
   return {
     type: SIGN_IN,
     payload: userId
@@ -22,6 +30,5 @@ export const createStream = formValues => async (dispatch) => {
     type: CREATE_STREAM,
     payload: response.data
   });
-
 
 };
