79af7a01e42b5d9698e6cec3f253d44297dad222
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-08 04:56:32 -0700

    added api architecture.

--- 9.twitch-clone/src/components/streams/StreamCreate.js
+++ 9.twitch-clone/src/components/streams/StreamCreate.js
@@ -2,16 +2,31 @@ import React from 'react';
 import {Field,reduxForm} from 'redux-form';
 
 class StreamCreate extends React.Component{
+  renderError ({error,touched}){
+    if(touched && error){
+      return (
+        <div className = "ui error message">
+          <div className = "header">
+            {error}
+          </div>
+        </div>
+      );
+    }
+  }
+
+
   //passes in formProps. we took out input
   //meta and label from formProps
-  renderInput({input, label, meta}){
+  renderInput = ({input, label, meta}) =>{
+    const className =  `field ${meta.error && meta.touched ?'error': ''}`
     return (
-      <div className="field">
+      <div className={className}>
       <label> {label} </label>
       <input
         {...input}
+        autoComplete="off"
       />
-      <div>{meta.error} </div>
+      {this.renderError(meta)}
       </div>
     );
   }
@@ -31,7 +46,7 @@ class StreamCreate extends React.Component{
 
         <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form">
+        className="ui form error">
           <Field
             name="title"
             component={this.renderInput}
