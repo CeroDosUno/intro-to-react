d4e21c081fa15113e877260a9153783bee2bd7b2
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-08 02:57:16 -0700

    created small form using redux form in streamcreate.js

--- 9.twitch-clone/src/components/streams/StreamCreate.js
+++ 9.twitch-clone/src/components/streams/StreamCreate.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import {Field,reduxForm} from 'redux-form';
 
-const StreamCreate = () =>{
-  return (
-      <div>
-      StreamCreate
+class StreamCreate extends React.Component{
+  //passes in formProps. we took out input from formProps
+  renderInput({input, label}){
+    return (
+      <div className="field">
+      <label> {label} </label>
+      <input
+        {...input}
+      />
       </div>
-  );
+    );
+  }
+
+
+  render(){
+    console.log(this.props);
+
+      return (
+
+        <form className="ui form">
+          <Field
+            name="Title"
+            component={this.renderInput}
+            label="Enter Title:"
+          />
+          <Field
+            name="Description"
+            component={this.renderInput}
+            label="Description:"
+          />
+
+        </form>
+
+      );
+    }
 }
 
-export default StreamCreate;
+//pass in only 1 form
+export default reduxForm({form:'simple'})(StreamCreate);
