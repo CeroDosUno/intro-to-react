918b81bfe60e6aae365028ad8cb38b8fab0b54f8
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-09 07:32:10 -0700

    notes on component isolation is made.

--- 9.twitch-clone/src/components/streams/StreamEdit.js
+++ 9.twitch-clone/src/components/streams/StreamEdit.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
+
 const StreamEdit = (props) =>{
   console.log(props);
 
   return (
       <div>
-      StreamEdit
+        StreamEdit
       </div>
   );
 }
 
-export default StreamEdit;
+//ownProps is the props being passed to streamedit.
+const mapStateToProps = (state, ownProps) => {
+  return{
+    stream: state.streams[ownProps.match.params.id]
+  };
+};
+
+export default connect(null)(StreamEdit);
