25dd4561596e00b831dff88d9f381d1f970fb5cc
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-09 08:09:45 -0700

    code refactor has been completed.

--- 9.twitch-clone/src/components/streams/StreamEdit.js
+++ 9.twitch-clone/src/components/streams/StreamEdit.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {fetchStream} from '../../actions';
 
+
 class StreamEdit extends React.Component{
   componentDidMount(){
     this.props.fetchStream(this.props.match.params.id);
---
+++ 9.twitch-clone/src/components/streams/StreamForm.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import {Field,reduxForm} from 'redux-form';
+
+class StreamForm extends React.Component{
+  renderError ({error,touched}){
+    if(touched && error){
+      return (
+        <div className = "ui error message">
+          <div className = "header">
+            {error}
+          </div>
+        </div>
+      );
+    }
+  }
+
+
+  //passes in formProps. we took out input
+  //meta and label from formProps
+  renderInput = ({input, label, meta}) =>{
+    const className =  `field ${meta.error && meta.touched ?'error': ''}`
+    return (
+      <div className={className}>
+      <label> {label} </label>
+      <input
+        {...input}
+        autoComplete="off"
+      />
+      {this.renderError(meta)}
+      </div>
+    );
+  }
+
+  //onsubmit is handled by redux form
+  //no need for event to prevent default.
+  //
+  onSubmit = (formValues) => {
+    // console.log(formValues);
+    //handles preventdefault()
+    this.props.onSubmit(formValues);
+  }
+
+
+  render(){
+
+      return (
+
+        <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error">
+          <Field
+            name="title"
+            component={this.renderInput}
+            label="Enter Title:"
+          />
+          <Field
+            name="description"
+            component={this.renderInput}
+            label="Enter Description:"
+          />
+          <button className="ui button primary" > Submit </button>
+        </form>
+
+      );
+    }
+}
+
+const validate = (formValues) =>{
+  const errors = {};
+  if(!formValues.title){
+    errors.title = 'Please enter a title.';
+  }
+  if(!formValues.description){
+    errors.description = 'Please enter a description.';
+  }
+
+  return errors;
+};
+
+
+//pass in only 1 form
+export default reduxForm({
+  form:'streamForm',
+  validate
+})(StreamForm);
