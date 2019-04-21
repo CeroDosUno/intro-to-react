d4befd1b091075e15f7ea3d4793efb31a9a90c43
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-21 14:19:32 -0700

    fixed bug. added new functional component in resourcelist.js

--- 11.app-with-hooks/src/components/ResourceList.js
+++ 11.app-with-hooks/src/components/ResourceList.js
@@ -4,12 +4,21 @@ import axios from 'axios';
 class ResourceList extends React.Component{
   state = {resources:[]};
 
+
+//called when component is rendered
   async componentDidMount(){
     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
     this.setState({resources: response.data});
-
   }
 
+//called when state changes
+  async componentDidUpdate(prevProps){
+    if(prevProps.resource !== this.props.resource){
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+      this.setState({resources: response.data});
+    }
+
+  }
 
   render() {
     return (
