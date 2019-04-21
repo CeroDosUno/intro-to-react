c9e5f49ecb7b0420d6050367069c857357f694cd
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-04-21 14:33:11 -0700

    refactored to  use useEffect component.

--- 11.app-with-hooks/src/components/ResourceList.js
+++ 11.app-with-hooks/src/components/ResourceList.js
@@ -1,33 +1,26 @@
-import React from'react';
+import React, {useState,useEffect} from'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component{
-  state = {resources:[]};
+const ResourceList = ({resource}) => {
+  const [resources,setResources] = useState([]);
 
 
-//called when component is rendered
-  async componentDidMount(){
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-    this.setState({resources: response.data});
-  }
+  const fetchResource = async (resource) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+    setResources(response.data);
+  };
 
-//called when state changes
-  async componentDidUpdate(prevProps){
-    if(prevProps.resource !== this.props.resource){
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-      this.setState({resources: response.data});
-    }
+  useEffect(() =>{
+    fetchResource(resource);
+  }, [])
 
-  }
 
-  render() {
     return (
       <div>
       In Resource <br/>
-      {this.state.resources.length}
+      {resources.length}
       </div>
     );
-  }
 }
 
 export default ResourceList;
